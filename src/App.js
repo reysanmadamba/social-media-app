0b65fca511dde334030b27f20829d9f2064de85b
Rey San Madamba <reysanmadamba00@gmail.com>
2022-10-23 19:10:27 +0800

    added appbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import AppBar from './components/navigation/AppBar'
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return <AppBar />;
+};
 
 export default App;
